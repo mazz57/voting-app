baec5fc20fee1da8ab7ce7c2eaae2af03197ddb8
MAZEN AHMED <mazenahmd18@gmail.com>
2025-02-15 02:25:22 +0530

    compleated userRouter and basic siginup and login stuff

--- db.js
+++ db.js
@@ -4,23 +4,29 @@ const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
-    age: Number,
-    adhaarNO: Number,
-    
+    adhaarNO: {type: Number, unique: true},
+    hasvoted : { type:Boolean, default:false}
 });
 
 const candidateSchema = new mongoose.Schema({
     name: String,
     age: Number,
     party: String,
-    votes: { type: Number, default: 0 },
+    voteCount: { type: Number, default: 0 },
+});
+
+const adminSchema = new mongoose.Schema({
+    adminID: mongoose.Schema.Types.ObjectId,
+    name: String,
+    password: String,
 });
 
 const userModel = mongoose.model("user" , userSchema);
 const candidateModel = mongoose.model("candidate" , candidateSchema);
-
+const adminModel = mongoose.model("admin" , adminSchema);       
 module.exports = {
-    User,
-    Candidate
+    userModel,
+    candidateModel,
+    adminModel
 };
 
